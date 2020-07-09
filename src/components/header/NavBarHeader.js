8c927bec773385d5c7f120542f83a5f01ebe9c71
mgracesb <mercysbarcel@gmail.com>
2020-07-09 17:24:12 +0200

    add language toggle

--- src/components/header/NavBarHeader.js
+++ src/components/header/NavBarHeader.js
@@ -3,9 +3,9 @@ import React from "react";
 const NavBarHeader = () => {
   return (
     <nav>
-      <ul>
-        <li></li>
-        <li>Info</li>
+      <ul className="navBarHeader">
+        <li className="homeButton active"></li>
+        <li className="infoButton">Info</li>
       </ul>
     </nav>
   );
