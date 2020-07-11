67388fc3d0df1a24770e57510afa24e7bb26f1dd
Mercy Shyu <mgrace.shyu@gmail.com>
2020-07-11 21:04:10 +0200

    add home photo and titles

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,16 @@
 import React from "react";
+import Photo from "../imgs/photo.png";
 
 const Home = () => {
-  return <section className="home"></section>;
+  return (
+    <section className="homeContainer">
+      <img src={Photo} alt="" className="photo" />
+      <div className="homeText">
+        <h2>mercy shyu</h2>
+        <p>Frontend developer</p>
+      </div>
+    </section>
+  );
 };
 
 export default Home;
