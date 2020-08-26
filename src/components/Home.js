2fd48fdd2b5a51952f765b86ba15130d7e0abd7d
Mercy Shyu <mgrace.shyu@gmail.com>
2020-08-26 19:50:02 +0200

    add home vid

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import vid from "../imgs/home-vid.mp4";
 
 const Home = () => {
   return (
     <section className="homeContainer">
+      <video src={vid} type="video/mp4" autoPlay loop></video>
       <div className="homeContent">
         <h2>mercy shyu</h2>
         <p className="subtitle">Frontend developer</p>
