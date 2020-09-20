f2a0dfd3cac87897e5a248785be20663eadaef62
Mercy Shyu <mgrace.shyu@gmail.com>
2020-09-20 17:49:17 +0200

    update production files

---
+++ docs/precache-manifest.b0771aac96e3b51b889af08bb0138298.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e6f2e312b7d81d7ded10ef1c9eb5d38",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0ffe2a8ef8f66caa2bd0",
+    "url": "./static/css/main.211aeffc.chunk.css"
+  },
+  {
+    "revision": "a1c0cbf502f7c6a7b5c1",
+    "url": "./static/js/2.f9792ac6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.f9792ac6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ffe2a8ef8f66caa2bd0",
+    "url": "./static/js/main.fa88a74f.chunk.js"
+  },
+  {
+    "revision": "83075419c04aeaaf02cb",
+    "url": "./static/js/runtime-main.bb42c72b.js"
+  },
+  {
+    "revision": "fea30a4b90eb192981a2471d623519f6",
+    "url": "./static/media/desescalada-mu.fea30a4b.png"
+  },
+  {
+    "revision": "566762e9bfae88b0749d58cb70ea9c5b",
+    "url": "./static/media/desescalada-vid.566762e9.mp4"
+  },
+  {
+    "revision": "bd3a9adb478cabfb72110d72de0cf822",
+    "url": "./static/media/digital-mu.bd3a9adb.png"
+  },
+  {
+    "revision": "bf50de172204dd7fe6991d7340ec46f4",
+    "url": "./static/media/digital-vid.bf50de17.mp4"
+  },
+  {
+    "revision": "78697ebd36c72f0654a6e5c148fb2d80",
+    "url": "./static/media/home-vid.78697ebd.mp4"
+  },
+  {
+    "revision": "a04e1361891af53ebaeba5e5d8b14373",
+    "url": "./static/media/kawaii-mu.a04e1361.png"
+  },
+  {
+    "revision": "2a66038ebf5909325816bf64df498bbd",
+    "url": "./static/media/kawaii-vid.2a66038e.mp4"
+  },
+  {
+    "revision": "e6e6cefe2fb5df3025daaba59d58ab5a",
+    "url": "./static/media/logo.e6e6cefe.png"
+  },
+  {
+    "revision": "9bd0845fd8f967a9ad686761f74e4a72",
+    "url": "./static/media/photo.9bd0845f.jpg"
+  }
+]);
